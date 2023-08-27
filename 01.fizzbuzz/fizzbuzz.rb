cf1899ccdd15a27d919bc6857f0038812a77d801
seicho <hw.th61@gmail.com>
2023-08-27 21:19:15 +0900

    課題提出

---
+++ 01.fizzbuzz/fizzbuzz.rb
@@ -0,0 +1,12 @@
+#!/usr/bin/env ruby
+(1..20).each {|n|
+  if n % 3 == 0 && n % 5 == 0
+    puts "FizzBuzz"
+  elsif n % 3 == 0
+    puts "Fizz"
+  elsif n % 5 == 0
+    puts "Buzz"
+  else
+    puts n
+  end
+}
