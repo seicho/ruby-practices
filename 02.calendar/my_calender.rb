5e34433fde84b16ce7175c37e94cc634d77303af
seicho <hw.th61@gmail.com>
2023-09-10 23:30:26 +0900

    コメント箇所の修正：表記を英語に変更

--- 02.calendar/my_calender.rb
+++ 02.calendar/my_calender.rb
@@ -3,65 +3,68 @@ require "date"
 opt = OptionParser.new
 parameters = ARGV.getopts("y:", "m:")
 
-def print_calender(date,e)
+def print_calender(first_day, last_day)
   counter = 0
-  first_line_indent = e 
-  today_day = 0
+  first_line_indent = first_day.wday 
+  today = 0
  
-  print "#{date.month}月 #{date.year}".center(21) + "\n"
+  print "#{first_day.strftime("%B")} #{first_day.year}".center(23) + "\n"
   
-  ["日","月","火","水","木","金","土"].each do |x|
-    print x.rjust(2)
+  ["Su","Mo","Tu","We","Th","Fr","Sa"].each do |x|
+    print x.rjust(3)
   end
-  print "\n"
+  puts
 
-  #1週目のインデントを作成
   first_line_indent.times do |x|
     print "".rjust(3)
     counter += 1
   end
 
-  #渡されたDateオブジェクトの年・月が現在の年・月と一致しているか判定
-  if date.year == Date.today.year && date.month == Date.today.month
-    today_day = Date.today.day
+  if first_day.year == Date.today.year && first_day.month == Date.today.month
+    today = Date.today
   end
  
-  (1..date.day).each do |x|
-    if x == today_day
-      print "\e[31m\e[47m#{x.to_s.rjust(3)}\e[0m"
+  (first_day..last_day).each do |x|
+    if x == today
+      print "\e[7m#{x.day.to_s.rjust(3)}\e[0m"
     else
-      print x.to_s.rjust(3)
+      print x.day.to_s.rjust(3)
     end
-    counter += 1
-    if counter == 7 
-      print "\n"
-      counter = 0
-    end
+    puts if x.saturday?
   end
-  puts ""
-  #最後に％が表示されてしまうのを防ぐために追加
+  puts
 end
 
-#1-12以外の月が入力された際の処理
-def isMonthValueInvalid?(parameters)
-  if parameters["m"].to_i < 1 || parameters["m"].to_i > 12
-    print "cal: #{parameters["m"]}は月を示す数字ではありません"
+def validate_month(month)
+  if month.to_i < 1 || month.to_i > 12
+    print "cal: #{month}は月を示す数字ではありません"
     exit
   end
 end
 
 #コマンドラインオプションに応じてprint_calenderに渡す引数を定義
-if parameters["y"] && parameters["m"]
-  isMonthValueInvalid?(parameters)
-  date =  Date.new(parameters["y"].to_i,parameters["m"].to_i,-1)
-  indent_for_first_week = Date.new(parameters["y"].to_i,parameters["m"].to_i,1).wday
-elsif parameters["m"]
-  isMonthValueInvalid?(parameters)
-  date = Date.new(Date.today.year,parameters["m"].to_i,-1)
-  indent_for_first_week = Date.new(Date.today.year,parameters["m"].to_i,1).wday
-else
-  date = Date.new(Date.today.year,Date.today.month,-1)
-  indent_for_first_week = Date.new(Date.today.year,Date.today.month,1).wday
-end
+#if parameters["y"] && parameters["m"]
+#  validate_month(parameters)
+#  date =  Date.new(parameters["y"].to_i,parameters["m"].to_i,-1)
+#  indent_for_first_week = Date.new(parameters["y"].to_i,parameters["m"].to_i,1).wday
+#elsif parameters["m"]
+#  validate_month(parameters)
+#  date = Date.new(Date.today.year,parameters["m"].to_i,-1)
+#  indent_for_first_week = Date.new(Date.today.year,parameters["m"].to_i,1).wday
+#else
+#  date = Date.new(Date.today.year,Date.today.month,-1)
+#  indent_for_first_week = Date.new(Date.today.year,Date.today.month,1).wday
+#end
 
-print_calender(date, indent_for_first_week)
+input_year = parameters['y']
+input_month = parameters['m']
+validate_month(input_month) if input_month
+today = Date.today
+
+year = input_year || today.year
+month = input_month || today.month
+
+first_day = Date.new(year.to_i, month.to_i, 1)
+last_day = Date.new(year.to_i, month.to_i, -1)
+
+print_calender(first_day, last_day)
