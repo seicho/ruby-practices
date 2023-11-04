1335e2494af1885ab8e028d9e0874a44e661c195
seicho <hw.th61@gmail.com>
2023-11-04 21:42:40 +0900

    refactoring

--- 04.ls/ls.rb
+++ 04.ls/ls.rb
@@ -1,23 +1,35 @@
 # frozen_string_literal: true
 
-path = ARGV[0] || '.'
-files = Dir.children(path).sort
-number_of_files = files.size
+def main
+  path = ARGV[0] || '.'
+  row = 3
+  files = Dir.children(path).sort
+  number_of_files = files.size
+  number_of_cols = (number_of_files % row).zero? ? numbe_of_files / row : number_of_files / row + 1
+  max_word_length = files.max_by(&:length).length
 
-row = 3
-number_of_cols = (number_of_files % row).zero? ? number_of_files / row : number_of_files / row + 1
-max_word_length = files.max_by(&:length).length
-output_format = Array.new(number_of_cols) { [] }
-index = 0
-(1..number_of_files).each do
-  index = 0 if index == number_of_cols
-  output_format[index] << files.shift
-  index += 1
+  finalized_filename_list = format_file_names(files, files.size, number_of_cols)
+  print_result(finalized_filename_list, max_word_length)
 end
 
-output_format.each do |col|
-  col.each do |word|
-    print word.ljust(max_word_length + 2)
+def format_file_names(files, number_of_files, number_of_cols)
+  output_format = Array.new(number_of_cols) { [] }
+  index = 0
+  (1..number_of_files).each do
+    index = 0 if index == number_of_cols
+    output_format[index] << files.shift
+    index += 1
   end
-  puts
+  output_format
 end
+
+def print_result(finalized_filename_list, max_word_length)
+  finalized_filename_list.each do |col|
+    col.each do |word|
+      print word.ljust(max_word_length + 2)
+    end
+    puts
+  end
+end
+
+main
