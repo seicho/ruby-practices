707308820a3cc789f560361a9672b1592bc3455e
seicho <hw.th61@gmail.com>
2023-11-25 21:21:22 +0900

    modification based on comment.

--- 04.ls/ls.rb
+++ 04.ls/ls.rb
@@ -4,23 +4,22 @@ def main
   path = ARGV[0] || '.'
   row = 3
   files = Dir.children(path).sort
-  number_of_files = files.size
-  number_of_cols = (number_of_files % row).zero? ? numbe_of_files / row : number_of_files / row + 1
   max_word_length = files.max_by(&:length).length
-
-  finalized_filename_list = format_file_names(files, files.size, number_of_cols)
-  print_result(finalized_filename_list, max_word_length)
+  formatted_filename_list = format_filename_list(files, row)
+  print_result(formatted_filename_list, max_word_length)
 end
 
-def format_file_names(files, number_of_files, number_of_cols)
-  output_format = Array.new(number_of_cols) { [] }
-  index = 0
+def format_filename_list(files, row)
+  number_of_files = files.size
+  number_of_cols = (number_of_files % row).zero? ? number_of_files / row : number_of_files / row + 1
+  formatted_filename_list = Array.new(number_of_cols) { [] }
+  allocated_files = 0
   (1..number_of_files).each do
-    index = 0 if index == number_of_cols
-    output_format[index] << files.shift
-    index += 1
+    allocated_files = 0 if allocated_files == number_of_cols
+    formatted_filename_list[allocated_files] << files.shift
+    allocated_files += 1
   end
-  output_format
+  formatted_filename_list
 end
 
 def print_result(finalized_filename_list, max_word_length)
