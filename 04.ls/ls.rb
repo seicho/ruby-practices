2945a96f962ffd296eb5d989af5fa7e9ec6e2ee1
seicho <hw.th61@gmail.com>
2023-11-27 21:35:45 +0900

    bug fix

--- 04.ls/ls.rb
+++ 04.ls/ls.rb
@@ -3,34 +3,45 @@
 def main
   path = ARGV[0] || '.'
   row = 3
-  files, max_word_length = analyze_directory(path)
-  formatted_filenames = format_filename_list(files, row)
-  print_result(formatted_filenames, max_word_length)
+  formatted_filenames = format_filenames(path, row)
+  margin_each_rows = calculate_margin(formatted_filenames, row)
+  print_result(formatted_filenames, margin_each_rows)
 end
 
-def analyze_directory(path)
-  files = Dir.children(path).sort
-  max_word_length = files.max_by(&:length).length
-  [files, max_word_length]
-end
-
-def format_filename_list(files, row)
-  number_of_files = files.size
-  number_of_cols = (number_of_files % row).zero? ? number_of_files / row : number_of_files / row + 1
+def format_filenames(path, row)
+  filenames = Dir.children(path).sort
+  number_of_filenames = filenames.size
+  number_of_cols = (number_of_filenames % row).zero? ? number_of_filenames / row : number_of_filenames / row + 1
   formatted_filenames = Array.new(number_of_cols) { [] }
-  allocated_files = 0
-  (1..number_of_files).each do
-    allocated_files = 0 if allocated_files == number_of_cols
-    formatted_filenames[allocated_files] << files.shift
-    allocated_files += 1
+  allocated_filenames = 0
+  (0..number_of_filenames - 1).each do |i|
+    if i == number_of_filenames - 1 && number_of_filenames == 4
+      formatted_filenames[0] << filenames[i]
+    else
+      allocated_filenames = 0 if allocated_filenames == number_of_cols
+      formatted_filenames[allocated_filenames] << filenames[i]
+      allocated_filenames += 1
+    end
   end
   formatted_filenames
 end
 
-def print_result(formatted_filenames, max_word_length)
+def calculate_margin(formatted_filenames, row)
+  margin_each_rows = Array.new(row, 0)
   formatted_filenames.each do |col|
+    (0..row - 2).each do |i|
+      margin_each_rows[i] = col[i].length if col[i] && margin_each_rows[i] < col[i].length
+    end
+  end
+  margin_each_rows
+end
+
+def print_result(formatted_filenames, margin_each_rows)
+  formatted_filenames.each do |col|
+    i = 0
     col.each do |word|
-      print word.ljust(max_word_length + 2)
+      print word.ljust(margin_each_rows[i] + 2)
+      i += 1
     end
     puts
   end
